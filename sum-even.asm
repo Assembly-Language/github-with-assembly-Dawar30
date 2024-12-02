6ebda3121a683fd81631367f1d719a692b5bf8fd
Dawar30 <meesumabbas891@gmail.com>
2024-12-02 12:13:37 +0500

    Task sum of even numbers

--- sum-even.asm
+++ sum-even.asm
@@ -1,18 +1,55 @@
-include irvine32.inc
+; Write an assembly program to find the sum of all even values in an array.
+;Calculate the sum of all even numbers in the array.
+;Display the sum.
+;Display how many total even numbers.
+
+include irvine32.inc 
 .data
+message1 db 'total even numbers:  ',0
+message db 'Sum of even values is:  ',0
+array WORD 1,3,5,7,9,31,6,4,10                         ;Declaration of array
+count word 0
 
-; array and other variables declaration here
+.code 
+main proc 
+call clrscr
+mov eax,0
+ mov esi,OFFSET array                                  ;moving offset of array in esi
+ mov ecx,lengthof array                                ;initializing ECX
+ 
+next: 
+ test WORD PTR [esi],1h                                ;Testing values with 0000 0000 0000 0001 or an odd number
+ pushfd                                                ;pushing registers for saving their states
+ add esi,TYPE array                                    ;moving to next index of array
+ popfd 
+ jz sum                                                ;if zero flag ON Jump to SUM label
+ again:
+ loop next
+jmp display                                            ;For directly moving to display
+ 
+sum:
+inc count                                              ;To count number of even numbers in array
+sub esi,TYPE array                                   ;To move back to previous index as in loop we moved to next element
+add ax,[esi]                                           ;Adding value in a register which is zero at initial state
+add esi,TYPE array                                     ;Again moving to next index after adding values
+jmp again
 
-.code
-main proc
+display:                                              ;To display the results
+mov edx, OFFSET message
+call writestring
+call writedec
+call crlf
+mov edx, OFFSET message1
+call writestring
+mov eax,0
+mov ax,count
+call writedec
 
 
-;code here
 
-
-
-exit
-main endp
-
-
+ exit 
+main endp 
 end main
+
+
+
